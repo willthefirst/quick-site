08369f2dbd3fd100efc56289be7b778584d468b1
Will Moritz <will@crowdfavorite.com>
2016-02-16 15:24:51 -0500

    basic editors with syntax highlighting

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,20 @@
 var app = {
+
   init: function() {
-    var editor = ace.edit("editor");
-    editor.setTheme("ace/theme/twilight");
-    editor.session.setMode("ace/mode/javascript");
+    this.initEditors();
+  },
+
+  // Which editors we will use
+  editors: ['html', 'javascript', 'css'],
+
+  // Initializes editor areas with Ace editor
+  initEditors: function() {
+    var editor;
+    for (var i = 0; i < this.editors.length; i++) {
+      editor = ace.edit('editor-' + this.editors[i]);
+      editor.setTheme("ace/theme/chrome");
+      editor.session.setMode("ace/mode/" + this.editors[i]);
+    };
   }
 }
 
